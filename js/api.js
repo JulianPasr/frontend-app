f23f0b7876edf2cc93a7d8c73230e46ad2f9af29
JulianPasr <juliancolegiatura321@gmail.com>
2024-11-19 09:17:44 -0500

    cambios importantes desde frontend app

--- js/api.js
+++ js/api.js
@@ -1,33 +1,40 @@
-const API_URL = "http://localhost:3000/api/products"
+const API_URL = "http://localhost:3000/api/products";
 
-
-
+// Obtener todos los productos
 export const getProducts = async () => {
-    const response = await  fetch(API_URL);
-    return response.json();
+  const response = await fetch(API_URL);
+  return response.json();
 };
 
+// Obtener Producto por ID
 export const getProductByID = async (id) => {
-    const response =await fetch(`${API_URL}/${ID}`);
-    return response.json();
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
 };
 
+// Crear un Producto
+export const addProduct = async (product) => {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(product)
+  });
+  return response.json();
+};
 
+// Actualizar un Producto
+export const updateProduct = async (id, product) => {
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(product)
+  });
+  return response.json();
+};
 
-export const addProduct = async (product)=> {
-    const response = await fetch (API_URL,{
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(product)
-    });
-    return response.json();
-}
-
-export const updateProduct = async (id,product)=> {
-    const response = await fetch (`${API_URL}/${ID}`,{
-        method: "PUT",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(product)
-    });
-    return response.json();
-}
+// Borrar un Producto
+export const deleteProduct = async (id) => {
+  return fetch(`${API_URL}/${id}`, {
+    method: "DELETE",
+  });
+};
